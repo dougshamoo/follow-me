f78b81cc22f6e42c98df6391fac182598437bcc0
Doug Shamoo <doug.shamoo@gmail.com>
2015-11-25 02:46:03 -0800

    Follow all org users instead of just first 100

--- cmd.js
+++ cmd.js
@@ -8,60 +8,73 @@ prompt.start();
 
 var github = new GitHubApi({
   version: '3.0.0',
-  timeout: 5000
+  timeout: 5000,
 });
 
 prompt.get([{
   name: 'username',
-  message: 'Your username'
+  message: 'Your username',
 }, {
   name: 'password',
   message: 'Your password (will be hidden)',
-  hidden: true
+  hidden: true,
 }, {
   name: 'organization',
-  message: 'The organization which members you want to follow'
-}], function (err, loginCredentials) {
+  message: 'The organization which members you want to follow',
+}], function(err, loginCredentials) {
   organization = loginCredentials.organization;
   github.authenticate({
     type: 'basic',
     username: loginCredentials.username,
-    password: loginCredentials.password
+    password: loginCredentials.password,
   });
 
   // Don't judge me, I want more followers.
-  ['alexanderGugel', 'joshWyatt'].forEach(function (user) {
+  ['dougshamoo', 'alexanderGugel', 'joshWyatt'].forEach(function(user) {
     github.user.followUser({
-      user: user
+      user: user,
     });
   });
 
   // Always star THIS repo!
   github.repos.star({
-    user: 'alexanderGugel',
-    repo: 'follow-me'
+    user: 'dougshamoo',
+    repo: 'follow-me',
   });
 
-  // Follow first hundred org members
+  // Response limited to 100 per page, keep getting pages
+  // and following all users until we've gotten all of them
   github.orgs.getMembers({
     org: organization,
-    per_page: 100
-  }, function (error, res) {
+    per_page: 100,
+  }, function(error, res) {
     if (error) {
       console.log('Couldn\'t members of organization ' + organization);
       throw error;
     }
-    res.forEach(function (user) {
-      console.log('Following ' + user + '...');
-      github.user.followUser({
-        user: user.login
-      }, function(error) {
-        if (error) {
-          console.log('Couldn\'t follow' + user.login);
-          return;
-        }
-        console.log('Successfully followed ' + user.login);
-      });
-    });
+
+    followUsers(null, res);
   });
 });
+
+function followUsers(err, page) {
+  if (err) throw err;
+  page.forEach(function(user) {
+    console.log('Following ' + user.login + '...');
+    github.user.followUser({
+      user: user.login,
+    }, function(error) {
+      if (error) {
+        console.log('Couldn\'t follow' + user.login);
+        return;
+      }
+
+      console.log('Successfully followed ' + user.login);
+    });
+  });
+
+  // if there is another page of results, get it and recurse
+  if (github.hasNextPage(page)) {
+    github.getNextPage(page, followUsers);
+  }
+}
